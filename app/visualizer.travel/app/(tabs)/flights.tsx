92e4696a4d0b12b5fecdcb0b89148a9c37dcaf72
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-10-31 22:09:06 -0600

    Review Changes

--- app/visualizer.travel/app/(tabs)/flights.tsx
+++ app/visualizer.travel/app/(tabs)/flights.tsx
@@ -1,8 +1,9 @@
+import styles from "@/styles/view";
 import { Text, View } from "react-native";
 
 export default function FlightsView() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Flights Screen</Text>
     </View>
   );
