af27545fcd31f21e5e894a374c07b65c50b6f3d6
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-11-02 13:19:22 -0600

    Closes #10

--- app/visualizer.travel/app/(tabs)/flights.tsx
+++ app/visualizer.travel/app/(tabs)/flights.tsx
@@ -1,10 +1,39 @@
 import styles from "@/styles/view";
-import { Text, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function FlightsView() {
   return (
     <View style={styles.container}>
-      <Text>Flights Screen</Text>
+      <View style={styles.row}>
+        <View style={styles.cell}></View>
+        <Text style={styles.cell}>Origin</Text>
+        <Text style={styles.cell}>Destination</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.cell}>Airport Code</Text>
+        <TextInput style={[styles.cell, styles.input]} />
+        <TextInput style={[styles.cell, styles.input]} />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.cell}>Time Zone</Text>
+        <TextInput style={[styles.cell, styles.input]} />
+        <TextInput style={[styles.cell, styles.input]} />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.cell}>Date</Text>
+        <TextInput style={[styles.cell, styles.input]} />
+        <TextInput style={[styles.cell, styles.input]} />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.cell}>Time</Text>
+        <TextInput style={[styles.cell, styles.input]} />
+        <TextInput style={[styles.cell, styles.input]} />
+      </View>
+      <View style={styles.row}>
+        <TouchableOpacity style={styles.button}>
+          <Text>Add Flight</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
