373708784d21576eb6826d32e6cceb77abfb71e7
kuro-neko.xyz <a.aleman.h@icloud.com>
2026-02-18 16:20:23 -0600

    Closes #29

---
+++ app/visualizer.travel/models/Airport.ts
@@ -0,0 +1,12 @@
+export interface Airport {
+  icao: string;
+  iata: string;
+  name: string;
+  city: string;
+  state: string;
+  country: string;
+  elevation: number;
+  lat: number;
+  lon: number;
+  tz: string;
+}
