34dde8a8943b079488ed0a69b6ae53e2c26050f9
Alejandro Alemán Hernández <alejandro@192.168.1.54>
2026-05-15 19:47:55 -0600

    Closes #78

--- server/seed.js
+++ server/seed.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const client = new Client({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
-  database: proces.env.PG_DATABASE,
+  database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
 });
