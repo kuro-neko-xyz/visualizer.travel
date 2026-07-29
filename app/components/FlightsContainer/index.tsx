316e189fca69737455d3106efd00709a8f5547db
kuro-neko-xyz <a.aleman.h@icloud.com>
2026-07-28 18:56:33 -0600

    Closes #60

--- app/components/FlightsContainer/index.tsx
+++ app/components/FlightsContainer/index.tsx
@@ -1,7 +1,7 @@
 import { Dispatch, FC, SetStateAction } from "react";
-import { ScrollView, StyleSheet, View, Text } from "react-native";
-import FlightInfo from "../FlightInfo";
+import { ScrollView, StyleSheet } from "react-native";
 import { Trips } from "@/models/Trip";
+import TripView from "../TripView";
 
 interface FlightsContainerProps {
   trips: Trips;
@@ -12,14 +12,7 @@ const FlightsContainer: FC<FlightsContainerProps> = ({ trips, setTrips }) => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
       {trips.map((trip) => (
-        <View key={trip.uuid}>
-          <Text style={styles.label}>{trip.name}</Text>
-          <View style={styles.flightsContainer}>
-            {trip.flights.map((flight) => (
-              <FlightInfo key={flight.id} flight={flight} setTrips={setTrips} />
-            ))}
-          </View>
-        </View>
+        <TripView key={trip.uuid} setTrips={setTrips} trip={trip} />
       ))}
     </ScrollView>
   );
@@ -35,19 +28,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     alignContent: "center",
     minHeight: "100%",
-  },
-  label: {
-    textAlign: "center",
-  },
-  flightsContainer: {
-    minWidth: "100%",
-    maxWidth: "100%",
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    alignItems: "center",
-    alignContent: "center",
   },
 });
 
