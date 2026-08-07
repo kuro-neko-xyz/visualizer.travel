e7ee0c8053cf3c6e7d3613aadda43638e6325d1a
kuro-neko-xyz <a.aleman.h@icloud.com>
2026-08-07 01:33:54 -0600

    Closes #101

--- app/helpers/flights/handleAddFlight.ts
+++ app/helpers/flights/handleAddFlight.ts
@@ -1,7 +1,7 @@
 import { Flight, Trips } from "@visualizer.travel/shared";
 import { randomUUID } from "expo-crypto";
 import { Dispatch, SetStateAction } from "react";
-import { getTimeZones } from "@vvo/tzdb";
+import parseDateString from "../shared/parseDateString";
 
 interface HandleAddFlightParams {
   arrivalDate: Date;
@@ -26,37 +26,27 @@ const handleAddFlight = ({
   originTimeZone,
   setTrips,
 }: HandleAddFlightParams) => {
-  const timeZones = getTimeZones();
+  const origin = parseDateString({
+    dummyDate: departureDate,
+    timeZone: originTimeZone,
+  });
 
-  const departureYear = departureDate.getFullYear();
-  const departureMonth = String(departureDate.getMonth() + 1).padStart(2, "0");
-  const departureDay = String(departureDate.getDate()).padStart(2, "0");
-  const departureHours = String(departureDate.getHours()).padStart(2, "0");
-  const departureMinutes = String(departureDate.getMinutes()).padStart(2, "0");
-  const originExtendedTimeZone = timeZones.find(
-    (tz) => originTimeZone === tz.name || tz.group.includes(originTimeZone),
-  );
+  const destination = parseDateString({
+    dummyDate: arrivalDate,
+    timeZone: destinationTimeZone,
+  });
 
-  const arrivalYear = arrivalDate.getFullYear();
-  const arrivalMonth = String(arrivalDate.getMonth() + 1).padStart(2, "0");
-  const arrivalDay = String(arrivalDate.getDate()).padStart(2, "0");
-  const arrivalHours = String(arrivalDate.getHours()).padStart(2, "0");
-  const arrivalMinutes = String(arrivalDate.getMinutes()).padStart(2, "0");
-  const destinationExtendedTimeZone = timeZones.find(
-    (tz) =>
-      destinationTimeZone === tz.name || tz.group.includes(destinationTimeZone),
-  );
   const flightData: Flight = {
     id: randomUUID(),
     origin: {
       airportCode: originAirport,
-      dateTime: `${departureYear}-${departureMonth}-${departureDay}T${departureHours}:${departureMinutes}:00${originExtendedTimeZone?.currentTimeFormat.substring(0, 6)}`,
-      timeZone: originExtendedTimeZone?.name || "",
+      dateTime: origin.dateString,
+      timeZone: origin.timeZone,
     },
     destination: {
       airportCode: destinationAirport,
-      dateTime: `${arrivalYear}-${arrivalMonth}-${arrivalDay}T${arrivalHours}:${arrivalMinutes}:00${destinationExtendedTimeZone?.currentTimeFormat.substring(0, 6)}`,
-      timeZone: destinationExtendedTimeZone?.name || "",
+      dateTime: destination.dateString,
+      timeZone: destination.timeZone,
     },
   };
   if (currentTrip === "") {
